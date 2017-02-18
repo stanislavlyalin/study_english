417ef122777403432b0b77df0054432bb7e47383
stanislavlyalin <lyalinstas@gmail.com>
2017-02-18 23:04:48 +0300

    Реализована чистая подстановка русских слов в оригинальный текст с сохранением исходного форматирования

--- replace.py
+++ replace.py
@@ -7,26 +7,33 @@ if __name__ == "__main__":
 
     with open('harry_potter.txt', 'r') as file:
         text = file.read()
-    text = text.split()
+    # text = text.split()
 
     known, unknown = load_dicts()
     translation = load_translation()
     trans = combine_dicts(unknown, translation)
 
-    pattern = re.compile('[\W_]+')
+    p1 = re.compile('[\W_]+')   # буквенно-цифровые символы
+    p2 = re.compile('[^\W_]+')  # пробелы, переносы строк и т.п.
     full_text = []
 
-    for word in text:
+    words = p1.split(text)
+    spaces = p2.split(text)
+
+    i = 0
+    for word in words:
 
         lower = word.lower()
-        lower = pattern.sub('', lower)
+        # lower = pattern.sub('', lower)
 
         if lower in unknown:
             idx = np.where(lower == trans[:,0])[0]
             if len(idx) > 0:
                 t = trans[idx[0],1]
-                full_text.append('%s /%s/' % (word, t))
+                full_text.append('%s%s/%s/' % (spaces[i], word, t))
         else:
-            full_text.append(word)
+            full_text.append('%s%s' % (spaces[i], word))
+
+        i += 1
     
-    write_to_file('ready.txt', ' '.join(full_text))
+    write_to_file('ready.txt', ''.join(full_text))
